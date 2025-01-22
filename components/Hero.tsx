b394c67a759e8f87faea63e8af3ba5d23abb5057
qammar1 <qammarshamraiz1@gmail.com>
2025-01-22 22:38:37 +0500

    updated

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,12 +1,14 @@
+
 import React from 'react'
+import { SparklesCore } from './ui/sparkles'
 
 function Hero() {
   return (
-    <div className='pb-20 pt-36'>
+    // <div className='pb-20 pt-36'>
         <div>
-
+<SparklesCore/>
         </div>
-    </div>
+    // </div>
   )
 }
 
