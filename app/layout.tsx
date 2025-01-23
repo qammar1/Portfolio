30e930c7bb2ac6363bc25d68aa8489cce3e96177
qammar1 <qammarshamraiz1@gmail.com>
2025-01-23 23:05:40 +0500

    updated

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-
+import { ThemeProvider } from "@/components/theme-provider";
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
@@ -24,11 +24,16 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+    <body>
+      <ThemeProvider
+        attribute="class"
+        defaultTheme="light"
+        enableSystem
+        disableTransitionOnChange
       >
         {children}
-      </body>
-    </html>
+      </ThemeProvider>
+    </body>
+  </html>
   );
 }
--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,15 +1,17 @@
-
-import React from 'react'
-import { SparklesCore } from './ui/sparkles'
+import React from "react";
+import { SparklesCore } from "./ui/sparkles";
 
 function Hero() {
   return (
     // <div className='pb-20 pt-36'>
-        <div>
-<SparklesCore/>
-        </div>
-    // </div>
-  )
+    <div>
+      <SparklesCore />
+      <div className="h-[50rem] w-full dark:bg-black bg-white  dark:bg-grid-white/[0.2] bg-grid-black/[0.2] relative flex items-center justify-center">
+        <div className="absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-black bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]" />
+      </div>
+    </div>
+    
+  );
 }
 
-export default Hero
+export default Hero;
